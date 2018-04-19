aac087f5480054b2231304153923129800aa91fa
chengxin <feijinshuliu@163.com>
2018-04-20 07:33:48 +0800

    安装fastclick插件

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import FastClick from 'fastclick'
+
 import router from './router/router'
 
-Vue.config.productionTip = false
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body)
+  }, false)
+}
 
 /* eslint-disable no-new */
 new Vue({
