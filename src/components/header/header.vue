d5971c13033001701cea62a3d1f010fd09d0c2a9
chengxin <feijinshuliu@163.com>
2018-04-21 09:28:41 +0800

    通过rem.js实现媒体查询

---
+++ src/components/header/header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <header id="head_top">
+        <slot name="logo"></slot>
+        <router-link :to="'/login'"  class="head_login">
+          <span class="login_span">登录|注册</span>
+        </router-link>
+      </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/style/mixin';
+
+  #head_top{
+    background-color: $blue;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    @include wh(100%, 1.95rem);
+  }
+  .head_goback{
+    left: 0.4rem;
+    @include wh(0.6rem, 1rem);
+    line-height: 2.2rem;
+    margin-left: .4rem;
+  }
+  .head_login{
+    right: 0.55rem;
+    @include sc(0.65rem, #fff);
+    @include ct;
+    .login_span{
+      color: #fff;
+    }
+    .user_avatar{
+      fill: #fff;
+      @include wh(.8rem, .8rem);
+    }
+  }
+  .title_head{
+    @include center;
+    width: 50%;
+    color: #fff;
+    text-align: center;
+    .title_text{
+      @include sc(0.8rem, #fff);
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+</style>
